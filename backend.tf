f9bd715f8de791f260ba8ac0646a86d21f6df8c6
EddieX64 <edik_rahimzhanov@mail.ru>
2022-08-08 20:30:24 +0600

    remove backend

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket               = "flaskex-tf-state-15205363"
-    key                  = "terraform.tfstate"
-    workspace_key_prefix = "template"
-    region               = "us-east-1"
-  }
-}
