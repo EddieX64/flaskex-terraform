bc3c2be042c1b85ec0f0755640c665d8c407c22f
EddieX64 <edik_rahimzhanov@mail.ru>
2022-08-12 15:43:34 +0600

    return default sg

--- module/networking/main.tf
+++ module/networking/main.tf
@@ -114,6 +114,33 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private.id
 }
 
+# Default Security Group of VPC
+resource "aws_security_group" "default" {
+  name        = "${var.environment}-default-sg"
+  description = "Default SG to alllow all traffic within the VPC"
+  vpc_id      = aws_vpc.vpc.id
+  depends_on = [aws_vpc.vpc]
+
+    ingress {
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+    self      = true
+  }
+
+  egress {
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+    self      = true
+  }
+
+  tags = {
+    Environment = "${var.environment}"
+  }
+}
+
+
 # Security group to allow inbound http on EC2 instances
 resource "aws_security_group" "allow_http" {
   name        = "allow_http"
